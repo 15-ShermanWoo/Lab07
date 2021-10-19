cfa4c1856030d737f6cb9891a5a8b11cca588b72
15ShermanWoo <shermanwwj@hotmail.com>
2021-10-19 16:02:01 +0800

    Updated PlayerScript
    Added Jump Feature For Duck

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,15 @@ public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    Rigidbody rb;
+    
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
@@ -16,6 +23,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             thisAnimation.Play();
         }
     }

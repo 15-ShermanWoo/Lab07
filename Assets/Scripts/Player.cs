0298c18aee2d1f22a2b0d55fc0366d1a452f05e3
15ShermanWoo <shermanwwj@hotmail.com>
2021-10-19 16:52:01 +0800

    Added Game Over

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -46,11 +46,13 @@ public class Player : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Boarder"))
         {
-            SceneManager.LoadScene("GameLose");
+            GameManager.thisManager.GameOver();
+            //SceneManager.LoadScene("GameLose");
         }
         else if (collision.gameObject.CompareTag("Obstacles"))
         {
-            SceneManager.LoadScene("GameLose");
+            GameManager.thisManager.GameOver();
+            //SceneManager.LoadScene("GameLose");
         }
     }
 }
